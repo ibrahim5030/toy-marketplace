13c3eb7892c7a928f754f24984be049e76f9716e
ibrahim5030 <rionjionn@gmail.com>
2023-05-22 23:28:03 +0600

    gallery section added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,11 @@
 import car from '../../assets/toycar.png';
 import useTitle from '../../hooks/useTitle';
+import windowCar from '../../assets/car.png';
+import babyCar from '../../assets/babycar.png';
+import pinkCar from '../../assets/pinkcar.jpg';
+import purpleCar from '../../assets/purplecar.png';
+import yellowCar from '../../assets/yellowcar.jpg';
+import blueCar from '../../assets/bluecar.png';
 
 const Home = () => {
     useTitle('Home');
@@ -18,6 +24,28 @@ const Home = () => {
                         <br /> 
                         For Car Lover Kids</h3>
                     </div>
+                </div>
+            </div>
+            {/* For Gallery Section */}
+            <h3 className="text-5xl text-sky-800 font-bold text-center mb-10">See The Gallery</h3>
+            <div className="w-10/12 mx-auto grid grid-cols-3 gap-6 mb-16">
+                <div className='outline outline-offset-4 outline-4 outline-sky-500 rounded'>
+                    <img src={windowCar} alt="" />
+                </div>
+                <div className='outline outline-offset-4 outline-4 outline-sky-500 rounded'>
+                    <img src={babyCar} alt="" />
+                </div>
+                <div className='outline outline-offset-4 outline-4 outline-sky-500 rounded'>
+                    <img src={pinkCar} alt="" />
+                </div>
+                <div className='outline outline-offset-4 outline-4 outline-sky-500 rounded'>
+                    <img src={purpleCar} alt="" />
+                </div>
+                <div className='outline outline-offset-4 outline-4 outline-sky-500 rounded'>
+                    <img src={yellowCar} alt="" />
+                </div>
+                <div className='outline outline-offset-4 outline-4 outline-sky-500 rounded'>
+                    <img src={blueCar} alt="" />
                 </div>
             </div>
         </div>
