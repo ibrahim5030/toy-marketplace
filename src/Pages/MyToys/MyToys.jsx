8c758c49824d110803e38e03c0bc66194bc79701
ibrahim5030 <rionjionn@gmail.com>
2023-05-23 15:17:09 +0600

    delete function added on My Toys page

--- src/Pages/MyToys/MyToys.jsx
+++ src/Pages/MyToys/MyToys.jsx
@@ -8,18 +8,18 @@ const MyToys = () => {
 
     useTitle('My Toys');
 
-    const allCarsToys = useLoaderData();  
-    
+    const allCarsToys = useLoaderData();
 
-    const {user} = useContext(AuthContext);      
-    
+
+    const { user } = useContext(AuthContext);
+
     const myCarToys = allCarsToys.filter(t => t.seller_email === user.email)
 
     const [myAddedToys, setMyAddedToys] = useState(myCarToys);
-    
+
     return (
         <div className="my-10">
-            <p className="text-5xl text-sky-800 text-center mb-10">See My Toys</p>          
+            <p className="text-5xl text-sky-800 text-center mb-10">See My Toys</p>
 
             {/* For Added Toys Section */}
             <div className="w-11/12 mx-auto">
@@ -42,8 +42,10 @@ const MyToys = () => {
                         </thead>
                         <tbody>
                             {
-                                myAddedToys.map(toy => <MyToysRow key={toy._id} 
-                                toy={toy}                                
+                                myAddedToys.map(toy => <MyToysRow key={toy._id}
+                                    toy={toy}
+                                    myAddedToys={myAddedToys}
+                                    setMyAddedToys={setMyAddedToys}
                                 ></MyToysRow>)
                             }
                         </tbody>
--- src/Pages/MyToys/MyToysRow.jsx
+++ src/Pages/MyToys/MyToysRow.jsx
@@ -1,9 +1,45 @@
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 
 
-const MyToysRow = ({ toy }) => {
+const MyToysRow = ({ toy, myAddedToys, setMyAddedToys }) => {
     const { _id, seller_email, description, seller_name, toy_name, picture_url, sub_category, price, available_quantity, rating } = toy;
+
+    const handleDelete = _id => {
+        console.log(_id);
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+
+
+                fetch(`http://localhost:5000/cars/${_id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            Swal.fire(
+                                'Deleted!',
+                                'Your toy has been deleted.',
+                                'success'
+                            )
+                            const remaining = myAddedToys.filter(to => to._id !== _id);
+                            setMyAddedToys(remaining);
+                        }
+                    })
+            }
+        })
+    }
 
     return (
         <tr>
@@ -17,7 +53,7 @@ const MyToysRow = ({ toy }) => {
             <td className="text-center">{available_quantity}</td>
             <td className="text-center"><div className="w-28 overflow-hidden">{description}</div></td>
             <td><Link to=""><button className="btn btn-active btn-secondary">Update</button></Link></td>
-            <td><button className="btn btn-active btn-secondary">Delete</button></td>
+            <td><button onClick={() => handleDelete(_id)} className="btn btn-active btn-secondary">Delete</button></td>
         </tr>
     );
 };
